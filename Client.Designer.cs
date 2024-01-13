fbd673d48d6e638e43e7d33154b89bd2e4aba1a0
HYPERPC <HYPERPC@DESKTOP-BIIOI1I>
2024-01-13 17:15:11 +0300

    Добавьте файлы проекта.

---
+++ Client.Designer.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+
+namespace DubnaClient
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.chatBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(171, 162);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(46, 162);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(100, 22);
+            this.messageBox.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.chatBox.FormattingEnabled = true;
+            this.chatBox.ItemHeight = 16;
+            this.chatBox.Location = new System.Drawing.Point(335, 28);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(418, 388);
+            this.chatBox.TabIndex = 3;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.ListBox chatBox;
+    }
+}
+
---
+++ Client.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DubnaClient
+{
+    public partial class Client : Form
+    {
+        private StreamWriter writer;
+        private StreamReader reader;
+        private NamedPipeClientStream client;
+
+        public Client()
+        {
+            InitializeComponent();
+            ConnectToServer();
+        }
+
+        private async void buttonSend_Click(object sender, EventArgs e)
+        {
+            string message = messageBox.Text;
+            // Сохраняем и отправляем сообщение на сервер, которое ввел пользователь
+            await writer.WriteLineAsync(message);
+            await writer.FlushAsync();
+            messageBox.Clear();
+        }
+
+        // Функция присоединения к серверу
+        private async void ConnectToServer()
+        {
+            client = new NamedPipeClientStream(".", "Dubna", PipeDirection.InOut, PipeOptions.Asynchronous);
+            await client.ConnectAsync();
+
+            reader = new StreamReader(client);
+            writer = new StreamWriter(client);
+
+            await ReadMessages();
+        }
+
+        // Функция, которая считывает сообщения со стрима и добавляет их в чат
+        private async Task ReadMessages()
+        {
+            while (true)
+            {
+                try
+                {
+                    string message = await reader.ReadLineAsync();
+
+                    if (!string.IsNullOrEmpty(message))
+                    {
+                        Invoke((MethodInvoker)(() => chatBox.Items.Add(message)));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
+
